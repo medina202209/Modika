55edaaa26bb838d17db1c02ab0e2bbbf73562888
Esteban Diaz <esteban_diaz96@hotmail.com>
2026-06-06 21:24:00 -0500

    se pone logo y se cambia el nombre de la empresa y se pone infoormacion adicional

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-zinc-950 text-stone-400 py-10 px-6 text-center text-sm border-t border-zinc-800">
-      <p className="mb-2 text-stone-100 font-semibold text-lg">Modika</p>
+      <p className="mb-2 text-stone-100 font-semibold text-lg">MODIKA</p>
       <p>© {new Date().getFullYear()} Todos los derechos reservados.</p>
     </footer>
   );
--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,17 +1,18 @@
 import { Link } from "react-router-dom";
+import Contacto from './Contacto';
 
 export default function Inicio() {
   return (
     <section className="min-h-screen flex items-center justify-center bg-gradient-to-br from-stone-200 to-stone-100 px-6">
       <div className="max-w-3xl text-center">
         <h1 className="text-5xl md:text-6xl font-extrabold text-zinc-900 leading-tight mb-6">
-          Modika <br />
+          MODIKA <br />
         </h1>
         <p className="text-lg text-stone-700 max-w-xl mx-auto mb-10">
           ⚡ ¡Algo increíble está llegando!
 
           Nuestra página se encuentra en proceso de actualización para traerte una experiencia totalmente renovada. 🚀
-          Vuelve pronto.
+          Vuelve pronto. Para mas informacion nos pueden contactar al correo contacto@modika.co
         </p>
       </div>
     </section>
